8752977c0266d60e1cff558b141b6205341ee773
ruinaz90 <rzhao9002@gmail.com>
2023-05-11 18:44:48 +0000

    fetch data

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,24 +1,13 @@
 <script>
-	import { enhance } from "$app/forms"
+    import { enhance } from "$app/forms"
 
-	export let data
-	export let form
+    export let data
+    let { session, supabase, profile, todosData } = data
+    let email = session.user.email
+    let loading = false
 
-	let { session, supabase, profile } = data
-
-  	let profileForm
-	let loading = false
-	let fullName = profile?.full_name ?? ""
-	let username = profile?.username ?? ""
-	let website = profile?.website ?? ""
-	let avatarUrl = profile?.avatar_url ?? ""
-
-	const handleSubmit = () => {
-		loading = true
-		return async () => {
-			loading = false
-		}
-	}
+    let todos = todosData
+    $: (todos = todosData)
 
 	const handleSignOut = () => {
 		loading = true
@@ -29,47 +18,17 @@
 	}
 </script>
 
-<div class="form-widget">
-	<form
-		class="form-widget"
-		method="post"
-		action="?/update"
-		use:enhance={handleSubmit}
-		bind:this={profileForm}
-	>
-		<div>
-			<label for="email">Email</label>
-			<input id="email" type="text" value={session.user.email} disabled />
-		</div>
-
-		<div>
-			<label for="fullName">Full Name</label>
-			<input id="fullName" name="fullName" type="text" value={form?.fullName ?? fullName} />
-		</div>
-
-		<div>
-			<label for="username">Username</label>
-			<input id="username" name="username" type="text" value={form?.username ?? username} />
-		</div>
-
-		<div>
-			<label for="website">Website</label>
-			<input id="website" name="website" type="url" value={form?.website ?? website} />
-		</div>
-
-		<div>
-			<input
-				type="submit"
-				class="button block primary"
-				value={loading ? 'Loading...' : 'Update'}
-				disabled={loading}
-			/>
-		</div>
-	</form>
-
+<div>
+    <p>Logged in as {email}</p>
 	<form method="post" action="?/signout" use:enhance={handleSignOut}>
 		<div>
 			<button class="button block" disabled={loading}>Sign Out</button>
 		</div>
 	</form>
+
+    <ul>
+        {#each todos as todo}
+          <li>{todo.task}</li>
+        {/each}
+      </ul>
 </div>
