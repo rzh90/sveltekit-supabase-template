e8ce5563f229dc9e444a8147b8acc6a49c8a5f1f
rzh90 <52875611+rzh90@users.noreply.github.com>
2023-05-11 15:11:02 -0400

    Update +layout.js

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,3 +1,9 @@
+/*
+* Code from the Supabase documentation to handle the session and the supabase object on the client-side
+* https://supabase.com/docs/guides/getting-started/tutorials/with-sveltekit
+* Copied from "src/routes/+layout.server.ts" section and changed to js
+*/
+
 import { invalidate } from "$app/navigation"
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public"
 import { createSupabaseLoadClient } from "@supabase/auth-helpers-sveltekit"
